c4484cf8087abef920ff60af0790c29821f72fba
kennethneil <macalandakenneth26@gmail.com>
2017-05-11 09:20:49 +0800

    Updated database

--- homeservice.sql
+++ homeservice.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.5.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2017 at 06:12 AM
--- Server version: 5.7.9
--- PHP Version: 5.6.16
+-- Generation Time: May 11, 2017 at 01:16 AM
+-- Server version: 5.7.11
+-- PHP Version: 5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,24 +26,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `services`
 --
 
-DROP TABLE IF EXISTS `services`;
-CREATE TABLE IF NOT EXISTS `services` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `provider` int(11) NOT NULL,
   `description` varchar(45) NOT NULL,
-  `category` enum('plumbing','handyman','electrical','cleaning') NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `provider` (`provider`),
-  KEY `category` (`category`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `category` enum('plumbing','handyman','electrical','cleaning') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `services`
 --
 
 INSERT INTO `services` (`id`, `name`, `provider`, `description`, `category`) VALUES
-(1, 'Juan', 2, 'Good in doing handyman work', 'handyman');
+(1, 'Juan', 2, '', 'handyman'),
+(2, 'Ferdinand', 1, '', 'plumbing'),
+(3, 'Jeafred', 3, '', 'electrical');
 
 -- --------------------------------------------------------
 
@@ -51,17 +49,13 @@ INSERT INTO `services` (`id`, `name`, `provider`, `description`, `category`) VAL
 -- Table structure for table `service_order`
 --
 
-DROP TABLE IF EXISTS `service_order`;
-CREATE TABLE IF NOT EXISTS `service_order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `service_order` (
+  `id` int(11) NOT NULL,
   `customer` int(11) NOT NULL,
   `provider` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `remarks` varchar(250) NOT NULL,
-  `status` enum('Pending','Accepted','Cancelled','In Process','Done') NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `customer` (`customer`),
-  KEY `provider` (`provider`)
+  `status` enum('Pending','Accepted','Cancelled','In Process','Done') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -70,23 +64,22 @@ CREATE TABLE IF NOT EXISTS `service_order` (
 -- Table structure for table `service_provider`
 --
 
-DROP TABLE IF EXISTS `service_provider`;
-CREATE TABLE IF NOT EXISTS `service_provider` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `service_provider` (
+  `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `contact` varchar(45) NOT NULL,
   `address` varchar(45) NOT NULL,
-  `description` varchar(250) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `description` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `service_provider`
 --
 
 INSERT INTO `service_provider` (`id`, `name`, `contact`, `address`, `description`) VALUES
-(1, 'SP1', '09054621485', 'Address', 'Plumbing'),
-(2, 'SP2', '09261574862', 'Address', 'Electical');
+(1, 'SP1', '09054621485', 'Trancoville, Baguio City', 'Plumbing'),
+(2, 'SP2', '09261574862', 'Dominican Hill, Baguio City', 'Electrical'),
+(3, 'SP3', '0926578643', 'Aurora Hill, Ambiong Road', 'Home Appliances');
 
 -- --------------------------------------------------------
 
@@ -94,18 +87,16 @@ INSERT INTO `service_provider` (`id`, `name`, `contact`, `address`, `description
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `customer_id` int(11) NOT NULL,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `first_name` char(45) NOT NULL,
   `last_name` char(45) NOT NULL,
   `user_status` enum('Pending','Active','Inactive') NOT NULL,
   `contact` varchar(12) NOT NULL,
-  `email_address` varchar(45) NOT NULL,
-  PRIMARY KEY (`customer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `email_address` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -116,6 +107,62 @@ INSERT INTO `users` (`customer_id`, `username`, `password`, `first_name`, `last_
 (2, 'user2', 'password', 'user', 'two', 'Pending', '09121234321', 'user2@gmail.com'),
 (3, 'user3', 'password', 'user', 'three', 'Pending', '09121234786', 'user3@gmail.com');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `provider` (`provider`),
+  ADD KEY `category` (`category`);
+
+--
+-- Indexes for table `service_order`
+--
+ALTER TABLE `service_order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer` (`customer`),
+  ADD KEY `provider` (`provider`);
+
+--
+-- Indexes for table `service_provider`
+--
+ALTER TABLE `service_provider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `service_order`
+--
+ALTER TABLE `service_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `service_provider`
+--
+ALTER TABLE `service_provider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
